3b0598a90c1ad4bd3a40b182a0a1e350f88b1532
G M Ganesh <gmganesh173@gmail.com>
2023-11-30 06:24:04 +0530

    local  url change

--- lib/config.dart
+++ lib/config.dart
@@ -1,6 +1,6 @@
 class Config {
   static const String appName = "Grocery App";
-  static const String appUrl = "http://localhost:4000";
+  static const String appUrl = "https://flutterecom.onrender.com/";
   static const String categoryApi = "api/category";
   static const int pageSized = 10;
   static const String currency = '@';
